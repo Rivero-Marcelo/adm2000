b9232ba5864d5c000fdb07de28b904848317fd48
Marcelo Rivero <rivero_marcelo@hotmail.com>
2022-07-14 02:16:21 -0300

    rutas

---
+++ public/rutas.php
@@ -0,0 +1,23 @@
+<?php
+
+require "../util/autoload.php";
+
+// Ruta::get($url, $funcion, $vista)
+
+
+//Router::Add("/login", "get", null, "LoginVista");
+
+//Router::Add("/login", "post", "UsuarioControlador::Login", null);
+
+//Router::Add("/logout", 'get', "logout", null);
+
+//Router::Add("/app/principal", "get", null, "Principal");
+
+
+Router::get("/login", null, "LoginVista");
+
+
+Router::get("/hola", null, "hola");
+
+//Router::get("/login", "caca::caca", "LoginVista");
+       
